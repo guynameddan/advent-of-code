598eeb69dd49dd200d2b6824b2783796a95aeb12
Dan Kim <guynameddan@gmail.com>
2023-07-23 01:56:27 -0600

    Play with readfile

--- day1/adventofcode_22_day_1.js
+++ day1/adventofcode_22_day_1.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 
-fs.readFile('adventofcode.com_2022_day_1_input.txt', (err, inputD) => {
+let sum = 0;
+let max = 0;
+
+// fs.readFile('adventofcode.com_2022_day_1_input.txt', (err, inputD) => {
+fs.readFile('test1.txt', (err, inputD) => {
+
     if (err) throw err;
-        console.log(inputD.toString());
+    
+    console.log(inputD.toString());
+    console.log(inputD.length);
+
+    // if inputD is a number add it to sum
+    // else compare the current sum to the max
+    // replace if greater than max
+
+    // Note: may need to use Math.
+
+    // if (inputD !== "") {
+    //     console.log("hello");
+    // } else {
+    //     return;
+    // }
 })
