b14387040f130b3d97f582591a1aeecfe2124044
Dan Kim <guynameddan@gmail.com>
2023-10-16 23:00:41 -0600

    Add day 1 2nd sol'n and create day2 files

--- day1/day1.js
+++ day1/day1.js
@@ -12,14 +12,28 @@ function readFileLines(filename) {
 let arr = readFileLines('input.txt');
 let currSum = 0;
 let max = 0;
+let sumArr = [];
 
-for (let i=0; i<=arr.length; i++) {
+// O(N)
+for (let i = 0; i <= arr.length; i++) {
     if (arr[i] === 0 || i === arr.length) {
         max = Math.max(currSum, max);
+        sumArr.push(currSum);
         currSum = 0;
     } else {
         currSum += arr[i];
     }
 }
 
-console.log(max);
+// console.log("max is " + max);
+
+// Sort array of sums in descending order, take the top 3 entries,
+// sum the top 3, and finally print
+
+// O(NlogN)
+sumArr.sort((a,b) => (b - a));
+
+// O(N)
+let top3Sum = sumArr.slice(0,3).reduce((sum, num) => sum + num);
+
+console.log("Sum of top 3: " + top3Sum);
---
+++ day2/day2.js
@@ -0,0 +1,16 @@
+/*
+1st col = opponent
+2nd col = me
+A, X = rock = 1 pt
+B, Y = paper = 2 pt
+C, Z = scissors = 3 pt
+
+round scoring:
+lost = 0 pt
+draw = 3 pt
+win = 6 pt
+
+total score = [selected shape] + [outcome of the round]
+*/
+
+// test2.txt total should be 15
