85ee4bb36b069b0c31d93eb67e003a14349a25b8
Dan Kim <guynameddan@gmail.com>
2023-10-17 23:03:57 -0600

    Edit from feedback

--- day1/day1.js
+++ day1/day1.js
@@ -3,37 +3,51 @@ const fs = require('fs');
 // Every line gets converted to a string because readFileSync returns
 // a buffer. Then strings have carriage return replaced, split into array
 // separated by /n, and finally converted to integers.
-function readFileLines(filename) {
+function readFileLinesToInt(filename) {
+    // let calorieList = fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/).map(Number);
     return fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/).map(Number);
 }
 
-// test1.txt sums should be 48559 and 41560
-// let arr = readFileLines('test1.txt');
-let arr = readFileLines('input.txt');
+function areNumbers(val) {
+    return typeof val === "number";
+}
+
+let arr = readFileLinesToInt('test2.txt');
+
+if (!arr.map(function(val) {return areNumbers(val);})) {
+    console.log("NaN. Please make sure all lines are numbers.");
+    return;
+}
+
+// A.map(function(a) {return doOpSingle2(a,theFlag);});
+
+
 let currSum = 0;
 let max = 0;
 let sumArr = [];
+let endOfArr = arr.length;
 
 // O(N)
-for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] === 0 || i === arr.length) {
+for (let idx = 0; idx <= endOfArr; idx++) {
+    if (arr[idx] === 0 || idx === endOfArr) {
         max = Math.max(currSum, max);
         sumArr.push(currSum);
         currSum = 0;
     } else {
-        currSum += arr[i];
+        currSum += arr[idx];
     }
 }
 
+// Answer to part 1:
 // console.log("max is " + max);
 
-// Sort array of sums in descending order, take the top 3 entries,
-// sum the top 3, and finally print
-
+// Sort array of sums in descending order
 // O(NlogN)
 sumArr.sort((a,b) => (b - a));
 
+// Sum the top 3
 // O(N)
 let top3Sum = sumArr.slice(0,3).reduce((sum, num) => sum + num);
 
+// and finally print part 2 sol'n
 console.log("Sum of top 3: " + top3Sum);
