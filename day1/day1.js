ea2c0fbe50584c08a9a68c0c89eec0d472b43ebc
Dan Kim <guynameddan@gmail.com>
2023-10-19 20:38:06 -0600

    Add error handling and cleanup

--- day1/day1.js
+++ day1/day1.js
@@ -4,52 +4,59 @@ const fs = require('fs');
 // a buffer. Then strings have carriage return replaced, split into array
 // separated by /n, and finally converted to integers.
 function readFileLinesToInt(filename) {
-    // let calorieList = fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/).map(Number);
-    console.log(fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/));
-    return fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/);
+    return fs.readFileSync(filename).toString().replace(/\r/g, "").split(/\n/).map(Number);
 }
 
-function areNumbers(val) {
-    console.log(typeof val === "number");
-    console.log(val);
-    return typeof val === "number";
+// verify each entry is a number
+function areNumbers(arr) {
+    // brute force method
+
+    let endOfArr = arr.length;
+
+    // TO DO: FIND CLEANER MORE EFFICIENT SOL'N?
+    for (let idx = 0; idx < endOfArr; idx++) {
+        if (isNaN(arr[idx])) {
+            throw new Error("1 or more lines NaN");
+        }
+    }
+
+    return arr;
 }
+// test output
+// let calorieList = readFileLinesToInt('test2.txt');
 
-let arr = readFileLinesToInt('test2.txt');
+let calorieList = readFileLinesToInt('input.txt');
 
-if (arr.map(function(val) {return areNumbers(val);}) === false) {
-    console.log("NaN. Please make sure all lines are numbers.");
-    return; // need to figure out how to end instead
+try {
+    areNumbers(calorieList);
+} catch (e) {
+    console.log(e);
+    return;
 }
-
-// A.map(function(a) {return doOpSingle2(a,theFlag);});
-
 
 let currSum = 0;
 let max = 0;
 let sumArr = [];
-let endOfArr = arr.length;
+let endOfArr = calorieList.length;
 
 // O(N)
 for (let idx = 0; idx <= endOfArr; idx++) {
-    if (arr[idx] === 0 || idx === endOfArr) {
+    if (calorieList[idx] === 0 || idx === endOfArr) {
         max = Math.max(currSum, max);
         sumArr.push(currSum);
         currSum = 0;
     } else {
-        currSum += arr[idx];
+        currSum += calorieList[idx];
     }
 }
 
 // Answer to part 1:
 // console.log("max is " + max);
 
-// Sort array of sums in descending order
-// O(NlogN)
+// Sort array of sums in descending order - O(NlogN)
 sumArr.sort((a,b) => (b - a));
 
-// Sum the top 3
-// O(N)
+// Sum the top 3 - O(N)
 let top3Sum = sumArr.slice(0,3).reduce((sum, num) => sum + num);
 
 // and finally print part 2 sol'n
