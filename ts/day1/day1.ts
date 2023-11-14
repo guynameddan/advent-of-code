059781a67c61c07481248b634a7938dbec1d2546
Dan Kim <guynameddan@gmail.com>
2023-11-13 20:15:30 -0700

    Get part 2 answer

--- ts/day1/day1.ts
+++ ts/day1/day1.ts
@@ -3,9 +3,12 @@ import * as fs from 'fs'
 // const calories = fs.readFileSync('./test1.txt', 'utf-8');
 const calories = fs.readFileSync('./input.txt', 'utf-8');
 
-const calArr = calories.split('\n').map(Number);
-let max = 0;
-let sum = 0;
+let calArr: number[] = calories.split('\n').map(Number);
+// calArr = calArr.split('\r').map(Number);
+
+let max: number = 0;
+let sum: number = 0;
+let top3: number[] = [];
 
 function findMax(cals: number[]): number {
     for (let i = 0; i < cals.length; i++) {
@@ -20,5 +23,28 @@ function findMax(cals: number[]): number {
     return max;
 }
 
-console.log(findMax(calArr));
+function findTop3(cals: number[]): number[] {
+    for (let i = 0; i < cals.length; i++) {
+        if (cals[i] === 0 || i === cals.length - 1) {
+            top3.push(sum);
+            sum = 0;
+        } else {
+            sum += cals[i];
+        }
+    }
+
+    return top3;
+}
+
+top3 = findTop3(calArr);
+
+// may reconsider using sort() cuz of shallow copy
+let ordered = top3.sort((a, b) => b - a).slice(0,3);
+
+let top3Sum = ordered.reduce((sum,num) => sum + num);
+
+// console.log(findMax(calArr));
 // console.log(calArr);
+
+console.log(ordered);
+console.log(top3Sum);
